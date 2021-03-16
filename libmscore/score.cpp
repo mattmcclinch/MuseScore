f2c3cbc3d2b919aad009009b52e3e65aed2e5be4
Matt McClinch <mattmcclinch@gmail.com>
2021-03-16 04:53:40 -0400

    Don't save unused images in MSCZ file

--- libmscore/score.cpp
+++ libmscore/score.cpp
@@ -74,6 +74,7 @@
 #include "breath.h"
 #include "instrchange.h"
 #include "synthesizerstate.h"
+#include "image.h"
 
 namespace Ms {
 
@@ -1432,6 +1433,9 @@ void Score::addElement(Element* element)
             case ElementType::HARMONY:
                   element->part()->updateHarmonyChannels(true);
                   break;
+            case ElementType::IMAGE:
+                  toImage(element)->setUsed(true);
+                  break;
 
             default:
                   break;
@@ -1595,6 +1599,9 @@ void Score::removeElement(Element* element)
                   break;
             case ElementType::HARMONY:
                   element->part()->updateHarmonyChannels(true, true);
+                  break;
+            case ElementType::IMAGE:
+                  toImage(element)->setUsed(false);
                   break;
 
             default:
